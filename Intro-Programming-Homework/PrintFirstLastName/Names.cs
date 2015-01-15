3d33831636a0c73698a02acbf27418fb9a5c06ee
zdzdz <zdzdz.link@yahoo.com>
2015-01-16 01:06:17 +0200

    Intro-to-Programming-HW

---
+++ Intro-Programming-Homework/PrintFirstLastName/Names.cs
@@ -0,0 +1,15 @@
+﻿// Problem 7. Print First and Last Name
+
+// Create console application that prints your first and last name, each at a separate line
+
+using System;
+
+class Names
+{
+    static void Main()
+    {
+        Console.WriteLine("Sam");
+        Console.WriteLine("Jones");
+    }
+}
+
